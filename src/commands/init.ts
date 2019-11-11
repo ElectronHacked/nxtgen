382d5881a339e9dbdae87b88f22e1f377fc13ca1
Phil Mashiane <mashianephil@gmail.com>
2019-11-12 00:30:43 +0200

    Added some flags and arguments for the init commands

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,4 +1,5 @@
 import { Command, flags } from '@oclif/command';
+import chalk = require('chalk');
 
 export default class Init extends Command {
   static description = 'generates a new project';
@@ -6,23 +7,32 @@ export default class Init extends Command {
   static flags = {
     help: flags.help({ char: 'h' }),
 
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({ char: 'n', description: 'name of the project' }),
+    // flag with no value (-a, --authentication)
+    authentication: flags.boolean({ char: 'a', description: 'include authentication' }),
 
-    // flag with no value (-f, --force)
-    force: flags.boolean({ char: 'f' }),
+    // flag with no value (-g, --googleAnalytics)
+    googleAnalytics: flags.boolean({ char: 'g', description: 'include Google Analytics' }),
+
+    // flag with no value (-a, --)
+    insights: flags.boolean({ char: 'i', description: 'include Application Insights' }),
   };
 
-  static args = [{ name: 'file' }];
+  static args = [{ name: 'name', required: true, description: 'name of the project' }];
 
   async run() {
     const { args, flags } = this.parse(Init);
-
-    const name = flags.name || 'world';
-    this.log(`hello ${name} from .\\src\\commands\\hello.ts`);
-
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`);
+    if (args.name) {
+      this.log(`The name of the project is: ${args.name}`);
+    } else {
+      this.error(`Please make sure that you provide the ${chalk.red.bold('name')} of the project`);
     }
+
+    const { authentication, googleAnalytics, insights } = flags;
+
+    this.log(`${authentication ? chalk.green.bold('Has') : chalk.red('Does not have')} authentication`);
+
+    this.log(`${googleAnalytics ? chalk.green.bold('Includes') : chalk.red('Does not include')} Google Analytics`);
+
+    this.log(`${insights ? chalk.green.bold('Includes') : chalk.red('Does not include')} Application Insights`);
   }
 }
--- src/commands/swag.ts
+++ src/commands/swag.ts
@@ -9,20 +9,19 @@ export default class Init extends Command {
     // flag with a value (-n, --name=VALUE)
     name: flags.string({ char: 'n', description: 'name of the project' }),
 
-    // flag with no value (-f, --force)
-    force: flags.boolean({ char: 'f' }),
+    // flag with no value (-a, --authentication)
+    authentication: flags.boolean({ char: 'a', description: 'include authentication' }),
+
+    // flag with no value (-g, --googleAnalytics)
+    googleAnalytics: flags.boolean({ char: 'g', description: 'include Google Analytics' }),
+
+    // flag with no value (-a, --)
+    insights: flags.boolean({ char: 'i', description: 'include Application Insights' }),
   };
 
   static args = [{ name: 'file' }];
 
   async run() {
     const { args, flags } = this.parse(Init);
-
-    const name = flags.name || 'world';
-    this.log(`hello ${name} from .\\src\\commands\\hello.ts`);
-
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`);
-    }
   }
 }
