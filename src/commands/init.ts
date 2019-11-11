3df4d00177710b22b3e6e9a3022967d3930f96bd
Phil Mashiane <mashianephil@gmail.com>
2019-11-11 23:13:28 +0200

    added the commands files to the commands folders

---
+++ src/commands/init.ts
@@ -0,0 +1,28 @@
+import { Command, flags } from '@oclif/command';
+
+export default class Init extends Command {
+  static description = 'generates a new project';
+
+  static flags = {
+    help: flags.help({ char: 'h' }),
+
+    // flag with a value (-n, --name=VALUE)
+    name: flags.string({ char: 'n', description: 'name of the project' }),
+
+    // flag with no value (-f, --force)
+    force: flags.boolean({ char: 'f' }),
+  };
+
+  static args = [{ name: 'file' }];
+
+  async run() {
+    const { args, flags } = this.parse(Init);
+
+    const name = flags.name || 'world';
+    this.log(`hello ${name} from .\\src\\commands\\hello.ts`);
+
+    if (args.file && flags.force) {
+      this.log(`you input --force and --file: ${args.file}`);
+    }
+  }
+}
