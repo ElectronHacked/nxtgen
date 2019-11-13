0d1b4c274b4db2b149760db8a4a6894776801918
Phil Mashiane <mashianephil@gmail.com>
2019-11-13 13:25:06 +0200

    Added some smart propmts on the init command

---
+++ src/config/index.ts
@@ -0,0 +1,9 @@
+export const WEB_TRACKING = {
+  ai: 'Application Insights',
+  ga: 'Google Analytics'
+}
+
+export const PREPROCESSOR = {
+  styledComponents: 'Styled Components',
+  scss: 'SCSS'
+}
